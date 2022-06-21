6b912c5631388f779031b9efce72e9def2f78a9a
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-06-21 17:38:15 +0530

    svsv

---
+++ jquery/base/base/js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  //  $(".red-box,.green-box").fadeOut(1000);
+  //  $(".red-box,.green-box").fadeIn(2000);
+  //  $(".red-box,.green-box").fadeTo(1000,0.5);
+  //  $(".red-box").fadeToggle()
+
+  //  $(".green-box").hide(2000);
+  //  $(".blue-box").slideUp(2000);  
+  //  $(".blue-box").slideDown(2000);
+   $(".blue-box").hide();
+   
+   $(".blue-box").slideDown(2000);
+   
+   
+
+});
