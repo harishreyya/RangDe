3f9ead731ff1021b8e52e6472f11fed2654c8dbb
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-06-28 09:27:44 +0530

    dtgbd

--- jquery/lightbox/js/script.js
+++ jquery/lightbox/js/script.js
@@ -1,53 +1,5 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-  //  $(".red-box,.green-box").fadeOut(1000);
-  //  $(".red-box,.green-box").fadeIn(2000);
-  //  $(".red-box,.green-box").fadeTo(1000,0.5);
-  //  $(".red-box").fadeToggle()
-
-  //  $(".green-box").hide(2000);
-  //  $(".blue-box").slideUp(2000);  
-  //  $(".blue-box").slideDown(2000);
-  //  $(".blue-box").slideToggle(2000);
-  //  $("p").hide()
-  //  $("p").slideDown(1000);
-   
-  // $(".blue-box").animate({
-  //   "margin-left":"+=200px"
-  // },2000,"linear");
-  
-  // $(".blue-box").animate({
-  //   "margin-left":"+=200px",
-  //   "opacity":"0",
-  //   "height":"50px",
-  //   "width": "50px"
-  // },2000);
-
-  // $("p").animate({
-  //     "font-family":"Inter",
-  //     "opacity":"0",
-  //     "font-size":"20px",
-  //     height:"50px",
-  //     width:"50px"
-  //   },2000);
-
-
-  // $(".red-box").fadeTo(1000,0.2);
-  // $(".green-box").delay(1000).fadeTo(1000,0.5);
-  // $(".blue-box").delay(2000).fadeTo(1000,0.8).fadeOut().delay(500).fadeIn();
-
-  //callback functions
-  // $(".red-box").fadeTo(3000,0.2,function(){
-  //   alert("whats up call back functions")
-  // });
-
-
-  // $(".red-box").fadeTo(1000,0.2,function(){
-  //      $(".green-box").fadeTo(1000,0.5,()=>{
-  //       $(".blue-box").fadeTo(1000,0.5).fadeOut().delay(500).fadeIn()
-  //      })
-  //   });
+  // $(".lightbox").delay(500).fadeIn(1000);
+  $(".lightbox").fadeIn(1000);
 
 });
