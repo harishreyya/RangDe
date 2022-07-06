8217d693484cebf189919949edf35f2a7f2e4bb4
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-07-06 20:10:57 +0530

    fvev

---
+++ visal/visal.js
@@ -0,0 +1,19 @@
+// var date = new Date();
+// var h = date.getHours();
+// var m = date.getMinutes();
+// var s = date.getSeconds();
+// console.log(h,m,s)
+
+// var name = "visal"
+// name = "keava"
+// name = "triveni"
+
+// console.log(name)
+
+// let name = "visal"
+// name = "kesava"
+// console.log(name)
+
+// const name = "visal"
+// name = "kesava"
+// console.log(name)
