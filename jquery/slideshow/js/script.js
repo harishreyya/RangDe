2dc93a30906a3bd318e9382f20a959c720d9fd92
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-07-10 20:00:26 +0530

    vetvE

--- jquery/slideshow/js/script.js
+++ jquery/slideshow/js/script.js
@@ -1,17 +1,17 @@
-// $(function() {
-//   var galleryImage = $(".gallery").find("img").first();
-//   var images = [
-//     "images/laptop-mobile_small.jpg",
-//     "images/laptop-on-table_small.jpg",
-//     "images/people-office-group-team_small.jpg",
-//   ]
-// let i = 0;
-// setInterval(function(){
-//   i=(i+1)%images.length;
-//   galleryImage.fadeOut(function(){
-//     $(this).attr("src",images[i]);
-//     $(this).fadeIn();
-//   });
-//   console.log(galleryImage.attr("src"));
-// },2000)
-// });
+$(function() {
+  var galleryImage = $(".gallery").find("img").first();
+  var images = [
+    "images/laptop-mobile_small.jpg",
+    "images/laptop-on-table_small.jpg",
+    "images/people-office-group-team_small.jpg",
+  ]
+let i = 0;
+setInterval(function(){
+  i=(i+1)%images.length;
+  galleryImage.fadeOut(function(){
+    $(this).attr("src",images[i]);
+    $(this).fadeIn();
+  });
+  console.log(galleryImage.attr("src"));
+},2000)
+});
